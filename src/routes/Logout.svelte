3cde6b1d27533c9dfb7bb915d8070935c10d2890
Vencelg <gazdavaclav@gmail.com>
2022-04-28 19:57:35 +0200

    logout fetch finished

--- src/routes/Logout.svelte
+++ src/routes/Logout.svelte
@@ -1,23 +1,33 @@
 <script>
     import { onMount } from "svelte";
 
-    onMount(async (e) => {
+    const logout = async (e) => {
         const token = "Bearer " + window.localStorage.getItem("token");
+        console.log(token);
 
         const res = await fetch("http://127.0.0.1:8000/api/auth/logout", {
             method: "GET",
             headers: {
-                "Content-Type": "application/json",
-                "Authentication": token,
+                Accept: "application/json",
+                Authorization: token,
             },
             mode: "cors",
         });
+        const json = await res.json();
+        const result = JSON.stringify(json);
+        let resultFinal = await JSON.parse(result);
 
-        //push("/login");
-    });
+        console.log(resultFinal);
+
+        if (res.ok) {
+            window.localStorage.removeItem("token");
+        }
+    };
 </script>
 
-<main />
+<main>
+    <button on:click={logout}>logout</button>
+</main>
 
 <style>
 </style>
