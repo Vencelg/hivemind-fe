ccc19e0d31b10d0a7e12510d92d7baea9dcc0c96
Vencelg <gazdavaclav@gmail.com>
2022-05-26 20:31:51 +0200

    api

--- src/routes/Logout.svelte
+++ src/routes/Logout.svelte
@@ -1,12 +1,8 @@
 <script>
     import { push, pop, replace } from "svelte-spa-router";
     import { user } from "../stores/store.js";
-<<<<<<< HEAD
     import api from "../scripts/api";
-
-=======
     import { onMount } from "svelte";
->>>>>>> 5780fc20007b3dce8ae6cbc157d2d674a6965a87
 
     onMount(async () => {
         const token = "Bearer " + window.localStorage.getItem("token");
